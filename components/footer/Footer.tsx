3fa12792d56729b055844bb8398d6f53d3cb6634
theWarlock <patrickvito2012@gmail.com>
2024-01-11 13:50:14 +0100

    add support

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,6 +5,13 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-800 p-4">
       <div className="text-center font-body capitalize p-3 text-white">
+        <p>
+          Contact{" "}
+          <span className="font-bold underline">
+            support@tradeswithkeithkaplan.com
+          </span>{" "}
+          for further support.
+        </p>
         <p>©2024 TradeSmith, LLC. All Rights Reserved.</p>
         <p>
           <Link href="/" className="underline text-blue-500">
