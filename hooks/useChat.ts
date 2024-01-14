ab5c5b982de51df31cac2fa51b42cd19966cab54
theWarlock <patrickvito2012@gmail.com>
2024-01-14 17:39:38 +0100

    changed the chat support api

--- hooks/useChat.ts
+++ hooks/useChat.ts
@@ -1,11 +1,28 @@
 import { useEffect } from "react";
 
+const CHAT_SCRIPT_URL = "//code.jivosite.com/widget/MQCwmFLKWf";
+
 export const useChat = () => {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = "//code.jivosite.com/widget/MQCwmFLKWf";
+    const loadChatScript = () => {
+      const chatScript = document.createElement("script");
+      chatScript.async = true;
+      chatScript.src = CHAT_SCRIPT_URL;
 
-    document.body.appendChild(script);
+      document.body.appendChild(chatScript);
+    };
+
+    // Load chat script on mount
+    loadChatScript();
+
+    // Cleanup function to remove script on component unmount
+    return () => {
+      const existingScript = document.querySelector(
+        `script[src="${CHAT_SCRIPT_URL}"]`
+      );
+      if (existingScript) {
+        document.body.removeChild(existingScript);
+      }
+    };
   }, []);
 };
